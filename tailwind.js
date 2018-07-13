55d17b2e33a9cd2ea1f4e2a21b109d9cb8420649
Vivian Guillen <me@vivian.do>
2018-07-13 19:09:06 -0400

    Updates tailwind version and config file.

--- tailwind.js
+++ tailwind.js
@@ -106,6 +106,7 @@ module.exports = {
     '2': '0.5rem',
     '3': '0.75rem',
     '4': '1rem',
+    '5': '1.25rem',
     '6': '1.5rem',
     '8': '2rem',
     '10': '2.5rem',
@@ -138,6 +139,7 @@ module.exports = {
     '2': '0.5rem',
     '3': '0.75rem',
     '4': '1rem',
+    '5': '1.25rem',
     '6': '1.5rem',
     '8': '2rem',
     '10': '2.5rem',
@@ -188,6 +190,7 @@ module.exports = {
     '2': '0.5rem',  //8
     '3': '0.75rem', //12
     '4': '1rem',    //16
+    '5': '1.25rem',
     '6': '1.5rem',  //24
     '7': '1.75rem',  //28
     '8': '2rem',    //32
@@ -211,6 +214,7 @@ module.exports = {
     '2': '0.5rem',  //8
     '3': '0.75rem', //12
     '4': '1rem',    //16
+    '5': '1.25rem',
     '6': '1.5rem',  //24
     '7': '1.75rem',  //28
     '8': '2rem',    //32
@@ -233,6 +237,7 @@ module.exports = {
     '2': '0.5rem',  //8
     '3': '0.75rem', //12
     '4': '1rem',    //16
+    '5': '1.25rem',
     '6': '1.5rem',  //24
     '7': '1.75rem',  //28
     '8': '2rem',    //32
@@ -253,6 +258,7 @@ module.exports = {
     'md': '0 4px 8px 0 rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.08)',
     'lg': '0 15px 30px 0 rgba(0,0,0,0.11), 0 5px 15px 0 rgba(0,0,0,0.08)',
     'inner': 'inset 0 2px 4px 0 rgba(0,0,0,0.06)',
+    'outline': '0 0 0 3px rgba(52,144,220,0.5)',
     'none': 'none',
   },
 
@@ -289,7 +295,8 @@ module.exports = {
     backgroundPosition: ['responsive'],
     backgroundRepeat: ['responsive'],
     backgroundSize: ['responsive'],
-    borderColors: ['responsive', 'hover'],
+    borderCollapse: [],
+    borderColors: ['responsive', 'hover', 'focus'],
     borderRadius: ['responsive'],
     borderStyle: ['responsive'],
     borderWidths: ['responsive'],
@@ -298,7 +305,7 @@ module.exports = {
     flexbox: ['responsive'],
     float: ['responsive'],
     fonts: ['responsive'],
-    fontWeights: ['responsive', 'hover'],
+    fontWeights: ['responsive', 'hover', 'focus'],
     height: ['responsive'],
     leading: ['responsive'],
     lists: ['responsive'],
@@ -309,18 +316,20 @@ module.exports = {
     minWidth: ['responsive'],
     negativeMargin: ['responsive'],
     opacity: ['responsive'],
+    outline: ['focus'],
     overflow: ['responsive'],
     padding: ['responsive'],
     pointerEvents: ['responsive'],
     position: ['responsive'],
     resize: ['responsive'],
-    shadows: ['responsive'],
+    shadows: ['responsive', 'hover', 'focus'],
     svgFill: [],
     svgStroke: [],
+    tableLayout: ['responsive'],
     textAlign: ['responsive'],
-    textColors: ['responsive', 'hover'],
+    textColors: ['responsive', 'hover', 'focus'],
     textSizes: ['responsive'],
-    textStyle: ['responsive', 'hover'],
+    textStyle: ['responsive', 'hover', 'focus'],
     tracking: ['responsive'],
     userSelect: ['responsive'],
     verticalAlign: ['responsive'],
